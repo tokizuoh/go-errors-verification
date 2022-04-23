373b7a353a298ea115f49f18fd4f644eb7e9f562
tokizuoh <tokizuoh@gmail.com>
2022-04-23 14:15:35 +0900

    fix

--- main.go
+++ main.go
@@ -1,27 +1,17 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 
 	"github.com/tokizuoh/go-errors-verification/p1"
 	"github.com/tokizuoh/go-errors-verification/p2"
 )
 
-// func New(text string) error {
-// 	return errorString(text)
-// }
-
-// type errorString string
-
-// func (e errorString) Error() string {
-// 	return string(e)
-// }
-
 func main() {
-
 	// errors で作成した同じ文字列のエラー比較
-	fmt.Println(p1.ErrInternal == p2.ErrInternal) // false
+	fmt.Println(errors.New("Internal error") == errors.New("Internal error")) // false
 
 	// 自作エラー (errorStringを構造体からstringに変更)で作成した同じ文字列のエラー比較
-	// fmt.Println(New("Internal error") == New("Internal error"))
+	fmt.Println(p1.ErrInternal == p2.ErrInternal) // true
 }
